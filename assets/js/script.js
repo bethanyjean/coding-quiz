bdb8c7b08b3623180c7d97d7d16b1059e949b7e3
Bethany Jean <bethanyjeanbg@gmail.com>
2022-03-27 19:40:32 -0400

    quiz works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,60 @@
-var timerE1 = document.getElementById('timer');
+var timerEl = document.getElementById('timer');
 var timeLeft = 75;
-var container = document.querySelector(".container");
+var currentQuestion = [];
+var pageContentEl = document.querySelector("#page-content");
 var highScore = [];
 var currentScore = 0;
-var questions = {
-    0: {
-        question: "Inside which HTML element do we put the JavaScript?",
+var i = 0;
+const questions = [
+    {
+        ask: "Inside which HTML element do we put the JavaScript?",
         answer1: "<script>",
         answer2: "<js>",
         answer3: "<javascript>",
         answer4: "<scripting>",
-        correct: 1,
+        correct: "answer1",
     },
-    1: {
-        question: 'How do you write "Hello World" in an alert box?',
+    {
+        ask: 'How do you write "Hello World" in an alert box?',
         answer1: 'alertBox(“Hello World”)',
         answer2: 'msgBox(“Hello World”)',
         answer3: 'alert(“Hello World”)',
         answer4: 'msg(“Hello World”)',
-        correct: 3,  
+        correct: "answer3",  
     },
-    2: {
-        question: 'How do you call a function named "myFunction"?',
+    {
+        ask: 'How do you call a function named "myFunction"?',
         answer1: 'myFunction()',
         answer2: 'call function myFunction()',
         answer3: 'call myFunction()',
         answer4: 'myFunction(call)',
-        correct: 1,
+        correct: "answer1",
     },
-    3: {
-        question: 'How to write an IF statement in JavaScript?',
+    {
+        ask: 'How to write an IF statement in JavaScript?',
         answer1: 'if (i === 5)',
         answer2: 'if i===5 then',
         answer3: 'if (i=5)',
         answer4: 'if i=5 then',
-        correct: 1, 
+        correct: "answer1", 
     },
-    4: {
-        question: 'How to write an IF statement for executing some code if “i” is NOT equal to 5?',
+    {
+        ask: 'How to write an IF statement for executing some code if “i” is NOT equal to 5?',
         answer1: 'if (i != 5)',
         answer2: 'if (i <> 5)',
         answer3: 'if i =! 5',
         answer4: 'if i<>5',
-        correct: 1,
+        correct: "answer1",
     },
-    5: {
-        question: 'Which event occurs when the user clicks on an HTML element?',
+    {
+        ask: 'Which event occurs when the user clicks on an HTML element?',
         answer1: 'onchange',
         answer2: 'onmouseover',
         answer3: 'onmouseclick',
         answer4: 'onclick',
-        correct: 4,
+        correct: "answer4",
     },
-}
+]
 
 
 // Create timer that counts down from 75 to 0, after each question time may be subtracted 
@@ -76,24 +78,38 @@ function timer() {
   }, 1000);
 }
 
-// // When start button is pressed, loop through questions.
-// startEl.addEventListener("click", funtion() {
-//     timer();
-//     startQuiz();
-// });
+var evaluateAnswer = function(event) {
+    var targetEl = event.target;
+    if (targetEl.value === currentQuestion.correct){
+        currentScore = currentScore + 10;
+    } else {
+        timeLeft = timeLeft - 25;
+    }
+    //increase i
+    i = i + 1;
+    askQuestion();
+}
 
-// // Loop through the quiz questions
-// function startQuiz() {
-//     var i = 0;
-//     do {
-//         //display question
-
-//         //validate answer
-//         //adjust timer if needed
-//         //increase i
-    
-//     } while (i < questions.length && timeLeft > 0);
-// }
+// Loop through the quiz questions
+function askQuestion() {
+    console.log(currentScore);
+    if(i < questions.length) {
+        //display question
+        
+        currentQuestion = questions[i];
+        var questionEl = document.querySelector("#ask");
+        var btnanswer1 = document.querySelector("#answer1");
+        var btnanswer2 = document.querySelector("#answer2");
+        var btnanswer3 = document.querySelector("#answer3");
+        var btnanswer4 = document.querySelector("#answer4");
+        questionEl.textContent = currentQuestion.ask;
+        btnanswer1.textContent = currentQuestion.answer1;
+        btnanswer2.textContent = currentQuestion.answer2;
+        btnanswer3.textContent = currentQuestion.answer3;
+        btnanswer4.textContent = currentQuestion.answer4;
+    }
+    else {endgame();}
+}
 
 var saveHighScore = function() {
     localStorage.setItem("highScore", JSON.stringify(highScore));
@@ -135,11 +151,15 @@ var loadHighScore = function() {
 function takeQuiz(){
     loadHighScore();
     timer();
+    askQuestion();
+    
 }
 
 // End Game Funtion - Display score and request initials
 function endgame() {
     console.log("game ended");
 }
+
+document.addEventListener("click", evaluateAnswer);
 
 takeQuiz();
